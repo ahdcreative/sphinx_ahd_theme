82b69d998769cb06ce8469aaec0a5ef653b06eb1
DarkArtek <darkartek@gmail.com>
2022-09-15 07:33:03 +0200

    Preparing release 1.0.1

---
+++ docs/examples/nesting/index.md
@@ -0,0 +1,11 @@
+# Nesting
+
+You can create subpages to a page by listing them in a `toctree`.
+
+```{toctree}
+---
+maxdepth: 2
+titlesonly:
+---
+child
+```
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  devtool: "source-map",
+  entry: {
+    theme: path.resolve(__dirname, "js/theme.js"),
+    fontawesome: path.resolve(__dirname, "sass/fontawesome.scss"),
+  },
+  output: {
+    path: path.resolve(__dirname, "sphinx_ahd_theme/static/dist"),
+    publicPath: "",
+    filename: "[name].js",
+  },
+  externals: {
+    jquery: "window.$",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(s?)css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2|svg)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "assets/[name].[ext]",
+          },
+        },
+      },
+    ],
+  },
+  // Disable performance hints – currently there are much more valuable
+  // optimizations for us to do outside of Webpack
+  performance: {
+    hints: false,
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendor",
+          chunks: "all",
+        },
+      },
+    },
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+};
